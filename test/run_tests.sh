2dc69e5bc790648e1cbc679b0d2a22f0aa9ca39b
Christopher Francis White <whitecf@bcs.org.uk>
2019-08-10 17:41:27 +0100

    Using parallel to intelligently run tests concurrently rather than simple bash for loop to launch them as background jobs.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,11 +2,6 @@
 cd `dirname $0`
 mkdir -p logs
 rm -f ./logs/*_test.log
-for TEST_FILE in *_test.mdb; do
-  TEST_NAME=`basename --suffix=.mdb ${TEST_FILE}`
-  echo "Launching ${TEST_NAME}"
-  mdb ./${TEST_NAME}.mdb > ./logs/${TEST_NAME}.log 2>&1 &
-done
-wait
+parallel "echo \"Launching {.}\";mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
 grep -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
 cd - > /dev/null
