067e355e8b8b5b9722c64e8e7d62cf814d8ceb2e
Christopher Francis White <whitecf@bcs.org.uk>
2019-08-15 15:06:51 +0100

    Rename test SCL reports.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,5 +3,5 @@ cd `dirname $0`
 mkdir -p logs
 rm -f ./logs/*_test.log
 parallel "echo \"Launching {.}\";mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
-grep -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
+grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
 cd - > /dev/null
