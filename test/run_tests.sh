44e950309bf080a2585b69c487fed83d83414a83
Christopher Francis White <whitecf@bcs.org.uk>
2019-08-28 19:56:55 +0100

    Print number of tests passed summary.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,4 +6,5 @@ echo "`ls *_test.mdb|wc -w` tests to run"
 parallel --bar "mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
 echo
 grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
+echo -e \\n`grep -h '_test: PASS' ./logs/*_test.log|wc -l` of `ls ./logs/*_test.log|wc -l` passed
 cd - > /dev/null
