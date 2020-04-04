1c678250f805b32d4b9e28994f23d2d7b9bcea41
Christopher Francis White <whitecf@bcs.org.uk>
2020-04-04 16:50:42 +0100

    Time parallel run of tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 mkdir -p logs
 rm -f ./logs/*_test.log
 echo "`ls *_test.mdb|wc -w` tests to run"
-parallel --bar "mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
+time parallel --bar "mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
 echo
 grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
 echo -e \\n`grep -h '_test: PASS' ./logs/*_test.log|wc -l` of `ls ./logs/*_test.log|wc -l` passed
