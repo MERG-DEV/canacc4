deeef4a0edcf8eb27d409d2e7645d2eb00aa8976
Christopher Francis White <whitecf@bcs.org.uk>
2020-05-04 18:30:29 +0100

    Sort test outcomes by alphabetical test name.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,6 +5,6 @@ rm -f ./logs/*_test.log
 echo "`ls *_test.mdb|wc -w` tests to run"
 time parallel --bar "mdb {} > ./logs/{.}.log 2>&1" ::: *_test.mdb
 echo
-grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log
+grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log|sort
 echo -e \\n`grep -h '_test: PASS' ./logs/*_test.log|wc -l` of `ls ./logs/*_test.log|wc -l` passed
 cd - > /dev/null
