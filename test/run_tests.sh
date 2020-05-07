4b9a2dc472d520c2c5df01bd6b72a89473d30e6b
Christopher Francis White <whitecf@bcs.org.uk>
2020-05-07 17:13:39 +0100

    Display progress rather than bar.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 mkdir -p logs
 rm -f ./logs/*_test.log
 echo "`ls *_test.mdb|wc -w` tests to run"
-time parallel --bar "mdb {} > ./logs/{.}.log 2>&1" ::: $(grep -E 'wait[ ]+for[ ]+[0-9]+[ ]+ms;' scl/*_test.scl|sort -n -r -k4|cut -d'/' -f2|cut -d':' -f1|sed s/scl/mdb/|awk '!seen[$0]++')
+time parallel --progress "mdb {} > ./logs/{.}.log 2>&1" ::: $(grep -E 'wait[ ]+for[ ]+[0-9]+[ ]+ms;' scl/*_test.scl|sort -n -r -k4|cut -d'/' -f2|cut -d':' -f1|sed s/scl/mdb/|awk '!seen[$0]++')
 echo
 grep -h -e '_test: PASS' -e '_test: FAIL' -e '_test: TIMEOUT' ./logs/*_test.log|sort
 echo -e \\n`grep -h '_test: PASS' ./logs/*_test.log|wc -l` of `ls ./logs/*_test.log|wc -l` passed
